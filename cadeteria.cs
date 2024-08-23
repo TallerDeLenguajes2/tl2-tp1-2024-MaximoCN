facb7a633c19637609665ef97983ffa979333879
“Maximo <maxicampos178@gmail.com>
2024-08-23 19:24:20 -0300

    estructurando

--- cadeteria.cs
+++ cadeteria.cs
@@ -1,4 +1,4 @@
-enum Estado
+public enum Estado
 {
     Aceptado,
     Cancelado,
@@ -10,20 +10,30 @@ public class Cliente{
     private string direccion;
     private int telefono;
     private string datosDirecc;
-    public Cliente(){
-
+    public Cliente(String nombre,string direccion,int telefono,string datosDirecc){
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.datosDirecc = datosDirecc;  
     }
 }
 
 public class Pedidos{
     private int numPedido;
     private string observacion;
-    public Cliente Clientes{get; set;}
-    private enum Estado;
-    public Pedidos(){
-
+    public Cliente Cliente{get; set;}
+    private  Estado EstadoPedido{get;set;}
+    public Pedidos(int numPedido,string observacion,Cliente cliente, Estado estadoPedido){
+        this.numPedido = numPedido;
+        this.observacion = observacion;
+        this.Cliente = cliente; // Composición: se inicializa en el constructor
+        this.EstadoPedido = estadoPedido;
     }
 
+public class VerDireccionCliente(){
+    
+}
+    
 }
 public class Cadete{
     private int id;
@@ -31,13 +41,25 @@ public class Cadete{
     private string direccion;
     private int telefono;
     public List<Pedidos> listadoPedidos{get;set;}
-    public Cadete(){
-
+    public Cadete(int id, string nombre,string direccion,int telefono){
+        this.id = id;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        listadoPedidos = new List<Pedidos>(); // Inicializa la lista de pedidos
     }
+   
 }
 
-public class cadeteria{
+public class Cadeteria{
     private string nombre;
     private int telefono;
     public List<Cadete> listaCadetes{get;set;}
-}
+     public Cadeteria(string nombre, int telefono)
+    {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        listaCadetes = new List<Cadete>(); // Inicializa la lista de cadetes
+    }
+     
+ }
